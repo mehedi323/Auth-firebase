d02a316a90fd80263e63169c9bfa67735fdd6baa
mehedi323 <dewanmehedi323@gmail.com>
2023-11-06 12:08:09 +0600

    components addad

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,17 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth';
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth';
 import app from '../../firebase/firebase.init';
 
 const Login = () => {
-  const [user, setUser]= useState()
+  const [user, setUser] = useState()
   const auth = getAuth(app)
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+  const gitHubProvider = new GithubAuthProvider()
+
+
   const handleLogIn = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
       .then(result => {
         const loginUser = result.user;
         console.log(loginUser);
@@ -18,28 +21,45 @@ const Login = () => {
         console.log(error);
       })
   }
-  const handleLogOut = () =>{
-    signOut(auth)
-    .then(result=>{
-      console.log(result);
-      setUser(null)
+
+  const handleGitHubLogIn = () =>{
+    signInWithPopup(auth , gitHubProvider)
+    .then(result =>{
+      const gitHubUser = result.user;
+      console.log(gitHubUser);
+      setUser(gitHubUser)
     })
-    .catch(error=>{
+    .catch(error =>{
       console.log(error);
     })
   }
+
+  const handleLogOut = () => {
+    signOut(auth)
+      .then(result => {
+        console.log(result);
+        setUser(null)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
   return (
     <div>
-      { user?
-      <button onClick={handleLogOut}>Log-Out</button> :
-      <button onClick={handleLogIn}>LogIn</button>}
-      { 
-      user && 
-      <div>
-        <h1>Name: {user.displayName}</h1>
-        <p>Email:{user.email}</p>
-        <img src={user.photoURL} alt="" />
-      </div>
+      {user ?
+        <button onClick={handleLogOut}>Log-Out</button> :
+        <>
+          <button onClick={handleLogIn}>LogIn</button>
+          <button onClick={handleGitHubLogIn}>GitHub-Login</button>
+        </>
+      }
+      {
+        user &&
+        <div>
+          <h1>Name: {user.displayName}</h1>
+          <p>Email:{user.email}</p>
+          <img src={user.photoURL} alt="" />
+        </div>
       }
     </div>
   );
--- src/components/SingIn/SingIn.jsx
+++ src/components/SingIn/SingIn.jsx
@@ -1,14 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth';
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth';
 import app from '../../firebase/firebase.init';
 
 const SingIn = () => {
   const [user, setUser] = useState();
   const auth = getAuth(app)
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+  const gitHubProvider = new GithubAuthProvider();
+
   const handleSingIn = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
       .then(result => {
         const logginUser = result.user;
         console.log(logginUser);
@@ -17,6 +19,18 @@ const SingIn = () => {
       .catch(error => {
         console.log('error', error);
       })
+  }
+
+  const handleGitHubLogin = () =>{
+    signInWithPopup(auth , gitHubProvider)
+    .then(result=>{
+      const gitHubUser = result.user;
+      console.log(gitHubUser);
+      setUser(gitHubUser)
+    })
+    .catch(error=>{
+      console.log(error);
+    })
   }
 
   const handleSingOut = () => {
@@ -33,7 +47,10 @@ const SingIn = () => {
     <div>
       { user?
       <button onClick={handleSingOut}>Sing-Out</button> :
-      <button onClick={handleSingIn}>Sing-In</button>
+      <>
+         <button onClick={handleSingIn}>Sing-In</button>
+         <button onClick={handleGitHubLogin}>GitHub-logIn</button>
+      </>
       }
       {user && <div>
         <h1>User-Name:{user.displayName}</h1>
